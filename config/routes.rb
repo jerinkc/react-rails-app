8dc2cfecbff5a2dcc70c63ad79187bfc261a6716
Jerin KC <jerinkc22@mail.com>
2020-04-09 13:02:27 +0530

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root "api/posts#index"
   namespace :api do
     resources :posts
   end
+  root 'hello_world#index'
 end
+# get 'posts/:id/edit', to: 'hello_world#index';
+# get 'posts/:id', to: 'hello_world#index';
+# get 'posts/new', to: 'hello_world#index';
+
